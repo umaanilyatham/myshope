a72961098b5275c143d334b0c57cd47a39ba6aad
EC2 Default User <ec2-user@ip-172-31-42-185.ap-south-1.compute.internal>
2026-05-19 06:57:57 +0000

    added dockerfile

--- myshope/views.py
+++ myshope/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-from flask import request
 from .models import things
 
 # Create your views here.
--- praticspro/settings.py
+++ praticspro/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import mysql.connector
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +77,8 @@ WSGI_APPLICATION = 'praticspro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'anil',
-        'USER': 'root',
-        'PASSWORD': 'Anil0537',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Password validation
